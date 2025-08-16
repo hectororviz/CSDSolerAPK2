6b965b2253b104a23cf9218013563838fc9547c3
hectororviz <hector.h.orviz@gmail.com>
2025-08-16 20:44:09 -0300

    Revert "Add theme mode switch with persisted preferences"

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'splash_screen.dart';
-import 'theme_notifier.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await themeNotifier.loadThemeMode();
+void main() {
   runApp(const MyApp());
 }
 
@@ -14,27 +11,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ValueListenableBuilder<ThemeMode>(
-      valueListenable: themeNotifier,
-      builder: (context, mode, _) {
-        return MaterialApp(
-          title: 'CSD Soler',
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-            textTheme: GoogleFonts.robotoTextTheme(
-              ThemeData.light().textTheme,
-            ),
-          ),
-          darkTheme: ThemeData.dark().copyWith(
-            textTheme: GoogleFonts.robotoTextTheme(
-              ThemeData.dark().textTheme,
-            ),
-          ),
-          themeMode: mode,
-          home: const SplashScreen(),
-          debugShowCheckedModeBanner: false,
-        );
-      },
+    return MaterialApp(
+      title: 'CSD Soler',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.robotoTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
+      home: const SplashScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
